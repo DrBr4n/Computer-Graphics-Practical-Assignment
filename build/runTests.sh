056fb4c9028145c916cdbbc7be0a8039575cb5fd
brunoSilva <brunocunhasilva2001@gmail.com>
2024-03-29 22:08:15 +0000

    - Added genTestModels and runTests for phase2

--- build/runTests.sh
+++ build/runTests.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
+if [ $1 == 1 ]
+then
 ./engine ../tests/test_files_phase_1/test_1_1.xml;
 ./engine ../tests/test_files_phase_1/test_1_2.xml;
 ./engine ../tests/test_files_phase_1/test_1_3.xml;
 ./engine ../tests/test_files_phase_1/test_1_4.xml;
 ./engine ../tests/test_files_phase_1/test_1_5.xml
+elif [ $1 == 2 ]
+then
+./engine ../tests/test_files_phase_2/test_2_1.xml;
+./engine ../tests/test_files_phase_2/test_2_2.xml;
+./engine ../tests/test_files_phase_2/test_2_3.xml;
+./engine ../tests/test_files_phase_2/test_2_4.xml;
+./engine ../tests/test_files_phase_2/solar_system.xml
+else 
+echo "Wrong option (available: 1, 2)"
+fi
