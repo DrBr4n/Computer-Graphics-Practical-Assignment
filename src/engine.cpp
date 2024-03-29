ece025a9dc73dfe15557c16a0036d5a94080e1bb
brunoSilva <brunocunhasilva2001@gmail.com>
2024-03-29 22:12:49 +0000

    - Added header file for engine.cpp - Renaming and reorganization of engine.cpp

--- src/engine.cpp
+++ src/engine.cpp
@@ -1,51 +1,250 @@
-#include "tinyxml2.h"
+#include "engine.h"
+
 #include <GL/gl.h>
 #include <GL/glut.h>
 #include <cmath>
+
 #include <fstream>
-#include <iostream>
 #include <sstream>
 #include <string>
 #include <vector>
 
+#include "tinyxml2.h"
+
 using namespace tinyxml2;
-using namespace std;
 
-struct Point3D {
-  GLfloat x;
-  GLfloat y;
-  GLfloat z;
-};
+struct Config *gpConfigData = new Config();
+struct Group *gpGroupRoot = NULL;
 
-struct XMLInfo {
-  int winWidth;
-  int winHeight;
-  Point3D camPos;
-  Point3D lookAt;
-  Point3D up;
-  GLdouble fov, near, far;
-  GLfloat alfa, beta, radius;
-};
+int main(int argc, char **argv) {
+  parseConfig(argv[1]);
+  // radius is not correct to camera position
+  gpConfigData->radius = 5.0f;
+  // asin(xmlInfo.camPos.y / xmlInfo.radius);
+  gpConfigData->beta = 0;
+  // asin(xmlInfo.camPos.x / (xmlInfo.radius * cos(xmlInfo.beta)));
+  gpConfigData->alfa = 0;
 
-struct Group {
-  Point3D translate = {0.0f, 0.0f, 0.0f};
-  Point3D rotate = {0.0f, 0.0f, 0.0f};
-  Point3D scale = {0.0f, 0.0f, 0.0f};
-  GLfloat angle = 0.0f;
-  std::vector<std::string> models;
-  std::vector<Point3D> points;
-  std::vector<struct Group> children;
-  std::vector<int> orderOfTransformations;
-};
+  // Init GLUT and the window
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+  glutInitWindowPosition(100, 100);
+  glutInitWindowSize(gpConfigData->winWidth, gpConfigData->winHeight);
+  glutCreateWindow(argv[1]);
 
-struct XMLInfo *xmlInfo = new XMLInfo();
-struct Group *groupTree = NULL;
+  // Required callback registry
+  glutDisplayFunc(renderScene);
+  glutReshapeFunc(changeSize);
+  glutSpecialFunc(processSpecialKeys);
 
-void initCamera() {
-  xmlInfo->radius = 5.0f;
-  xmlInfo->beta = 0; // asin(xmlInfo.camPos.y / xmlInfo.radius);
-  xmlInfo->alfa =
-      0; // asin(xmlInfo.camPos.x / (xmlInfo.radius * cos(xmlInfo.beta)));
+  // OpenGL settings
+  glEnable(GL_DEPTH_TEST);
+  glEnable(GL_CULL_FACE);
+  glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+  // Enter GLUT's main cycle
+  glutMainLoop();
+
+  return 0;
+}
+
+void changeSize(int w, int h) {
+  // Prevent a divide by zero, when window is too short
+  // (you cant make a window with zero width).
+  if (h == 0)
+    h = 1;
+
+  // Compute window's aspect ratio
+  float ratio = w * 1.0 / h;
+
+  // Set the projection matrix as current
+  glMatrixMode(GL_PROJECTION);
+  // Load Identity Matrix
+  glLoadIdentity();
+
+  // Set the viewport to be the entire window
+  glViewport(0, 0, w, h);
+
+  // Set perspective
+  gluPerspective(gpConfigData->fov,
+                 gpConfigData->winWidth * 1.0 / gpConfigData->winHeight,
+                 gpConfigData->near, gpConfigData->far);
+
+  // Return to the model view matrix mode
+  glMatrixMode(GL_MODELVIEW);
+}
+
+void renderScene(void) {
+  // Clear buffers
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+  // Set the camera
+  glLoadIdentity();
+  gluLookAt(gpConfigData->camPos.x, gpConfigData->camPos.y,
+            gpConfigData->camPos.z, gpConfigData->lookAt.x,
+            gpConfigData->lookAt.y, gpConfigData->lookAt.z, gpConfigData->up.x,
+            gpConfigData->up.y, gpConfigData->up.z);
+
+  // Draw
+  drawAxes();
+
+  glColor3f(1.0f, 1.0f, 1.0f);
+
+  drawGroup(gpGroupRoot);
+
+  // End of frame
+  glutSwapBuffers();
+}
+
+void processSpecialKeys(int key, int xx, int yy) {
+  switch (key) {
+  case GLUT_KEY_RIGHT:
+    gpConfigData->alfa -= 0.1;
+    break;
+  case GLUT_KEY_LEFT:
+    gpConfigData->alfa += 0.1;
+    break;
+  case GLUT_KEY_UP:
+    gpConfigData->beta += 0.1f;
+    if (gpConfigData->beta > 1.5f)
+      gpConfigData->beta = 1.5f;
+    break;
+  case GLUT_KEY_DOWN:
+    gpConfigData->beta -= 0.1f;
+    if (gpConfigData->beta < -1.5f)
+      gpConfigData->beta = -1.5f;
+    break;
+  case GLUT_KEY_PAGE_DOWN:
+    gpConfigData->radius -= 0.1f;
+    if (gpConfigData->radius < 0.1f)
+      gpConfigData->radius = 0.1f;
+    break;
+  case GLUT_KEY_PAGE_UP:
+    gpConfigData->radius += 0.1f;
+    break;
+  }
+  spherical2Cartesian();
+  glutPostRedisplay();
+}
+
+void spherical2Cartesian() {
+  gpConfigData->camPos.y = gpConfigData->radius * sin(gpConfigData->beta);
+  gpConfigData->camPos.x =
+      gpConfigData->radius * cos(gpConfigData->beta) * sin(gpConfigData->alfa);
+  gpConfigData->camPos.z =
+      gpConfigData->radius * cos(gpConfigData->beta) * cos(gpConfigData->alfa);
+}
+
+int parseConfig(const char *config) {
+  XMLDocument xmlDoc;
+  if (xmlDoc.LoadFile(config) != 0)
+    return XML_ERROR_FILE_COULD_NOT_BE_OPENED;
+
+  XMLNode *pRoot = xmlDoc.FirstChild();
+  if (pRoot == nullptr)
+    return XML_ERROR_FILE_READ_ERROR;
+
+  XMLElement *pElem = pRoot->FirstChildElement("window");
+  pElem->QueryIntAttribute("width", &gpConfigData->winWidth);
+  pElem->QueryIntAttribute("height", &gpConfigData->winHeight);
+
+  pElem = pRoot->FirstChildElement("camera");
+  XMLElement *pListElem = pElem->FirstChildElement("position");
+  pListElem->QueryFloatAttribute("x", &gpConfigData->camPos.x);
+  pListElem->QueryFloatAttribute("y", &gpConfigData->camPos.y);
+  pListElem->QueryFloatAttribute("z", &gpConfigData->camPos.z);
+
+  pListElem = pListElem->NextSiblingElement("lookAt");
+  pListElem->QueryFloatAttribute("x", &gpConfigData->lookAt.x);
+  pListElem->QueryFloatAttribute("y", &gpConfigData->lookAt.y);
+  pListElem->QueryFloatAttribute("z", &gpConfigData->lookAt.z);
+
+  pListElem = pListElem->NextSiblingElement("up");
+  pListElem->QueryFloatAttribute("x", &gpConfigData->up.x);
+  pListElem->QueryFloatAttribute("y", &gpConfigData->up.y);
+  pListElem->QueryFloatAttribute("z", &gpConfigData->up.z);
+
+  pListElem = pListElem->NextSiblingElement("projection");
+  pListElem->QueryDoubleAttribute("fov", &gpConfigData->fov);
+  pListElem->QueryDoubleAttribute("near", &gpConfigData->near);
+  pListElem->QueryDoubleAttribute("far", &gpConfigData->far);
+
+  XMLElement *pGroup = pRoot->FirstChildElement("group");
+  if (pGroup != NULL) {
+    gpGroupRoot = parseGroup(pGroup);
+  }
+
+  return 0;
+}
+
+struct Group *parseGroup(XMLElement *pGroupElem) {
+  XMLElement *pElem = NULL;
+  struct Group *pGroup = new Group();
+  // Check if <transform> exists, if so use for loop to retrieve first
+  // transform and iterate over its siblings
+  if ((pElem = pGroupElem->FirstChildElement("transform")) != NULL) {
+    for (pElem = pElem->FirstChildElement(); pElem != NULL;
+         pElem = pElem->NextSiblingElement()) {
+      Vector3D newVector;
+      pElem->QueryFloatAttribute("x", &newVector.x);
+      pElem->QueryFloatAttribute("y", &newVector.y);
+      pElem->QueryFloatAttribute("z", &newVector.z);
+      const char *transformType = pElem->Name();
+      if (std::strcmp(transformType, "translate") == 0) {
+        pGroup->orderOfTransformations.emplace_back(1);
+        pGroup->translate = newVector;
+      } else if (std::strcmp(transformType, "rotate") == 0) {
+        pGroup->orderOfTransformations.emplace_back(2);
+        pElem->QueryFloatAttribute("angle", &pGroup->angle);
+        pGroup->rotate = newVector;
+      } else if (std::strcmp(transformType, "scale") == 0) {
+        pGroup->orderOfTransformations.emplace_back(3);
+        pGroup->scale = newVector;
+      }
+    }
+  }
+
+  // Check if <models> exists, if so use for loop to retrieve first model
+  // and iterate over its siblings
+  if ((pElem = pGroupElem->FirstChildElement("models")) != NULL) {
+    for (pElem = pElem->FirstChildElement("model"); pElem != NULL;
+         pElem = pElem->NextSiblingElement("model")) {
+      pGroup->models.emplace_back(pElem->FindAttribute("file")->Value());
+    }
+  }
+
+  // Create a child for every group child of current group and emplace it in the
+  // current group children vector
+  struct Group *pChild = NULL;
+  for (pGroupElem = pGroupElem->FirstChildElement("group"); pGroupElem != NULL;
+       pGroupElem = pGroupElem->NextSiblingElement("group")) {
+    pChild = parseGroup(pGroupElem);
+    pGroup->children.push_back(*pChild);
+  }
+
+  return pGroup;
+}
+
+std::vector<struct Vector3D> parseModels(std::vector<std::string> modelNames) {
+  std::vector<struct Vector3D> points;
+  // NOTE: Might be a bug
+  Vector3D point;
+
+  for (const auto &modelName : modelNames) {
+    std::ifstream File("../3d/" + modelName);
+    std::string line;
+
+    while (getline(File, line)) {
+      std::istringstream lineStream(line);
+      lineStream >> point.x;
+      lineStream >> point.y;
+      lineStream >> point.z;
+      points.push_back(point);
+    }
+
+    File.close();
+  }
+  return points;
 }
 
 void drawAxes() {
@@ -68,125 +267,7 @@ void drawAxes() {
   glEnd();
 }
 
-// NOTE:: translate-1, rotate-2, scale-3
-// Geometric transformations can only exist inside a group(v)
-// and are applied to all models and subgroups            (?)
-// There can only be one transformation of each type      (-)
-// Note: the order of the geometric transforms is relevant(v)
-struct Group *parseGroups(XMLElement *pGroupElem) {
-  XMLElement *pElem = NULL;
-  struct Group *pGroup = new Group();
-  // Check if <transform> exists, if so use for loop to retrieve first
-  // transform and iterate over its siblings
-  if ((pElem = pGroupElem->FirstChildElement("transform")) != NULL) {
-    for (pElem = pElem->FirstChildElement(); pElem != NULL;
-         pElem = pElem->NextSiblingElement()) {
-      Point3D newPoint;
-      pElem->QueryFloatAttribute("x", &newPoint.x);
-      pElem->QueryFloatAttribute("y", &newPoint.y);
-      pElem->QueryFloatAttribute("z", &newPoint.z);
-      const char *transformType = pElem->Name();
-      if (std::strcmp(transformType, "translate") == 0) {
-        pGroup->orderOfTransformations.push_back(1);
-        pGroup->translate = newPoint;
-      } else if (std::strcmp(transformType, "rotate") == 0) {
-        pGroup->orderOfTransformations.push_back(2);
-        pElem->QueryFloatAttribute("angle", &pGroup->angle);
-        pGroup->rotate = newPoint;
-      } else if (std::strcmp(transformType, "scale") == 0) {
-        pGroup->orderOfTransformations.push_back(3);
-        pGroup->scale = newPoint;
-      }
-    }
-  }
-
-  // Check if <models> exists, if so use for loop to retrieve first model
-  // and iterate over its siblings
-  if ((pElem = pGroupElem->FirstChildElement("models")) != NULL) {
-    for (pElem = pElem->FirstChildElement("model"); pElem != NULL;
-         pElem = pElem->NextSiblingElement("model")) {
-      std::string modelName = pElem->FindAttribute("file")->Value();
-      pGroup->models.emplace_back(modelName);
-    }
-  }
-
-  // Create a child for every group child of current group and emplace it in the
-  // current group children vector
-  struct Group *pChild = NULL;
-  for (pGroupElem = pGroupElem->FirstChildElement("group"); pGroupElem != NULL;
-       pGroupElem = pGroupElem->NextSiblingElement("group")) {
-    pChild = parseGroups(pGroupElem);
-    pGroup->children.emplace_back(*pChild);
-  }
-
-  return pGroup;
-}
-
-int readConfig(const char *config) {
-  XMLDocument xmlDoc;
-  if (xmlDoc.LoadFile(config) != 0)
-    return XML_ERROR_FILE_COULD_NOT_BE_OPENED;
-
-  XMLNode *pRoot = xmlDoc.FirstChild();
-  if (pRoot == nullptr)
-    return XML_ERROR_FILE_READ_ERROR;
-
-  XMLElement *pElem = pRoot->FirstChildElement("window");
-  pElem->QueryIntAttribute("width", &xmlInfo->winWidth);
-  pElem->QueryIntAttribute("height", &xmlInfo->winHeight);
-
-  pElem = pRoot->FirstChildElement("camera");
-  XMLElement *pListElem = pElem->FirstChildElement("position");
-  pListElem->QueryFloatAttribute("x", &xmlInfo->camPos.x);
-  pListElem->QueryFloatAttribute("y", &xmlInfo->camPos.y);
-  pListElem->QueryFloatAttribute("z", &xmlInfo->camPos.z);
-
-  pListElem = pListElem->NextSiblingElement("lookAt");
-  pListElem->QueryFloatAttribute("x", &xmlInfo->lookAt.x);
-  pListElem->QueryFloatAttribute("y", &xmlInfo->lookAt.y);
-  pListElem->QueryFloatAttribute("z", &xmlInfo->lookAt.z);
-
-  pListElem = pListElem->NextSiblingElement("up");
-  pListElem->QueryFloatAttribute("x", &xmlInfo->up.x);
-  pListElem->QueryFloatAttribute("y", &xmlInfo->up.y);
-  pListElem->QueryFloatAttribute("z", &xmlInfo->up.z);
-
-  pListElem = pListElem->NextSiblingElement("projection");
-  pListElem->QueryDoubleAttribute("fov", &xmlInfo->fov);
-  pListElem->QueryDoubleAttribute("near", &xmlInfo->near);
-  pListElem->QueryDoubleAttribute("far", &xmlInfo->far);
-
-  XMLElement *pGroup = pRoot->FirstChildElement("group");
-  if (pGroup != NULL) {
-    groupTree = parseGroups(pGroup);
-  }
-
-  return 0;
-}
-
-vector<struct Point3D> readModels(vector<string> fileNames) {
-  vector<struct Point3D> points;
-
-  for (string fileName : fileNames) {
-    ifstream File("../3d/" + fileName);
-    string line;
-
-    while (getline(File, line)) {
-      istringstream lineStream(line);
-      Point3D vector3d;
-      lineStream >> vector3d.x;
-      lineStream >> vector3d.y;
-      lineStream >> vector3d.z;
-      points.emplace_back(vector3d);
-    }
-
-    File.close();
-  }
-  return points;
-}
-
-void drawGroupTree(struct Group *group) {
-
+void drawGroup(struct Group *group) {
   for (const auto &order : group->orderOfTransformations) {
     switch (order) {
     case 1:
@@ -202,129 +283,17 @@ void drawGroupTree(struct Group *group) {
     }
   }
 
-  group->points = readModels(group->models);
+  group->points = parseModels(group->models);
 
   glBegin(GL_TRIANGLES);
-  for (struct Point3D point : group->points) {
+  for (const auto &point : group->points) {
     glVertex3f(point.x, point.y, point.z);
   }
   glEnd();
 
   for (struct Group child : group->children) {
     glPushMatrix();
-    drawGroupTree(&child);
+    drawGroup(&child);
     glPopMatrix();
   }
-}
-
-void spherical2Cartesian() {
-  xmlInfo->camPos.x = xmlInfo->radius * cos(xmlInfo->beta) * sin(xmlInfo->alfa);
-  xmlInfo->camPos.y = xmlInfo->radius * sin(xmlInfo->beta);
-  xmlInfo->camPos.z = xmlInfo->radius * cos(xmlInfo->beta) * cos(xmlInfo->alfa);
-}
-
-void processSpecialKeys(int key, int xx, int yy) {
-  switch (key) {
-  case GLUT_KEY_RIGHT:
-    xmlInfo->alfa -= 0.1;
-    break;
-  case GLUT_KEY_LEFT:
-    xmlInfo->alfa += 0.1;
-    break;
-  case GLUT_KEY_UP:
-    xmlInfo->beta += 0.1f;
-    if (xmlInfo->beta > 1.5f)
-      xmlInfo->beta = 1.5f;
-    break;
-  case GLUT_KEY_DOWN:
-    xmlInfo->beta -= 0.1f;
-    if (xmlInfo->beta < -1.5f)
-      xmlInfo->beta = -1.5f;
-    break;
-  case GLUT_KEY_PAGE_DOWN:
-    xmlInfo->radius -= 0.1f;
-    if (xmlInfo->radius < 0.1f)
-      xmlInfo->radius = 0.1f;
-    break;
-  case GLUT_KEY_PAGE_UP:
-    xmlInfo->radius += 0.1f;
-    break;
-  }
-  spherical2Cartesian();
-  glutPostRedisplay();
-}
-
-void changeSize(int w, int h) {
-  // Prevent a divide by zero, when window is too short
-  // (you cant make a window with zero width).
-  if (h == 0)
-    h = 1;
-
-  // Compute window's aspect ratio
-  float ratio = w * 1.0 / h;
-
-  // Set the projection matrix as current
-  glMatrixMode(GL_PROJECTION);
-  // Load Identity Matrix
-  glLoadIdentity();
-
-  // Set the viewport to be the entire window
-  glViewport(0, 0, w, h);
-
-  // Set perspective
-  gluPerspective(xmlInfo->fov, xmlInfo->winWidth * 1.0 / xmlInfo->winHeight,
-                 xmlInfo->near, xmlInfo->far);
-
-  // Return to the model view matrix mode
-  glMatrixMode(GL_MODELVIEW);
-}
-
-void renderScene(void) {
-  // Clear buffers
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-  // Set the camera
-  glLoadIdentity();
-  gluLookAt(xmlInfo->camPos.x, xmlInfo->camPos.y, xmlInfo->camPos.z,
-            xmlInfo->lookAt.x, xmlInfo->lookAt.y, xmlInfo->lookAt.z,
-            xmlInfo->up.x, xmlInfo->up.y, xmlInfo->up.z);
-
-  // Draw
-  drawAxes();
-
-  glColor3f(1.0f, 1.0f, 1.0f);
-
-  drawGroupTree(groupTree);
-
-  // End of frame
-  glutSwapBuffers();
-}
-
-int main(int argc, char **argv) {
-
-  readConfig(argv[1]);
-  initCamera();
-  debugGroupTree(groupTree);
-
-  // Init GLUT and the window
-  glutInit(&argc, argv);
-  glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
-  glutInitWindowPosition(100, 100);
-  glutInitWindowSize(xmlInfo->winWidth, xmlInfo->winHeight);
-  glutCreateWindow(argv[1]);
-
-  // Required callback registry
-  glutDisplayFunc(renderScene);
-  glutReshapeFunc(changeSize);
-  glutSpecialFunc(processSpecialKeys);
-
-  // OpenGL settings
-  glEnable(GL_DEPTH_TEST);
-  glEnable(GL_CULL_FACE);
-  glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-
-  // Enter GLUT's main cycle
-  glutMainLoop();
-
-  return 0;
 }
---
+++ src/engine.h
@@ -0,0 +1,56 @@
+#ifndef ENGINE_H
+#define ENGINE_H
+
+#include "tinyxml2.h"
+#include <GL/gl.h>
+#include <string>
+#include <vector>
+
+using namespace tinyxml2;
+
+struct Vector3D {
+  GLfloat x;
+  GLfloat y;
+  GLfloat z;
+};
+
+struct Config {
+  int winWidth;
+  int winHeight;
+  Vector3D camPos;
+  Vector3D lookAt;
+  Vector3D up;
+  GLdouble fov, near, far;
+  GLfloat alfa, beta, radius;
+};
+
+struct Group {
+  std::vector<struct Group> children;
+  Vector3D translate;
+  Vector3D scale;
+  Vector3D rotate;
+  GLfloat angle;
+  std::vector<int> orderOfTransformations;
+  std::vector<std::string> models;
+  std::vector<struct Vector3D> points;
+};
+
+void changeSize(int w, int h);
+
+void renderScene(void);
+
+void processSpecialKeys(int key, int xx, int yy);
+
+void spherical2Cartesian();
+
+int parseConfig(const char *config);
+
+struct Group *parseGroup(XMLElement *pGroupElem);
+
+std::vector<struct Vector3D> parseModels(std::vector<std::string> modelNames);
+
+void drawAxes();
+
+void drawGroup(struct Group *group);
+
+#endif
